8fbd696a69e52156b94bbda1f53b5baa9e1ffa4c
Petr Korolev <sky4winder@gmail.com>
2017-12-11 18:18:57 +0300

    add lib

---
+++ Example/Pods/Target Support Files/ios-secp256k1/ios-secp256k1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ios_secp256k1 : NSObject
+@end
+@implementation PodsDummy_ios_secp256k1
+@end
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,6 +1,6 @@
 import UIKit
 import XCTest
-import ios-secp256k1
+//import ios-secp256k1
 
 class Tests: XCTestCase {
     
